522d59815352cbbe171ad1c089921fd69f601632
Svetlin Parev <eta-sys@users.noreply.github.com>
2017-04-22 10:23:19 +0300

    Update Example1_blink.ml

--- Example1_blink.ml
+++ Example1_blink.ml
@@ -8,4 +8,4 @@ E
 
 This is the program shown in Demo.avi.  
 For small Pic processors use 'Send file line by line' option in 'MySerial' to give them time to write rows to Flash.
-For processors with File System set use command 'W"main.ml" to upload example as execute ml.
+For processors with File System set use command W"main.ml" to upload example as execute ml.
