2dfb49b132ae860c36c26498dcf90cffd4dc5724
Svetlin Parev <eta-sys@users.noreply.github.com>
2017-04-22 11:00:09 +0300

    Update Example4_counter.ml

--- Example4_counter.ml
+++ Example4_counter.ml
@@ -1,4 +1,4 @@
-This program ilustrates 'call' to a subroutine. Pic16F628A does not suppotr 'call's, so replace C with G 'goto' for him.  
+This program ilustrates 'call' to a subroutine. Pic16F628A does not suppotr 'call's, so replace C with G 'goto' for it.  
 Program uses strings, so if you don't have them repare last row leaving just '!V1'.
 
 V1=0
