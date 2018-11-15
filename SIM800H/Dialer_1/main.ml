59e045412e06201f13b8ffda89b5406e5a026978
Svetlin Parev <eta-sys@users.noreply.github.com>
2018-11-15 16:26:01 +0200

    Update main.ml

--- SIM800H/Dialer_1/main.ml
+++ SIM800H/Dialer_1/main.ml
@@ -1,8 +1,3 @@
-#HD: add a switch between BLUE TERMINALS Z1 and +12V
-#SW: replace string "0011223344" with your phone number
-#Switching switch On/Off you'll receive a SMS.
-#Use code as template to create you own alarm system.
-
 O=0,I{1,2,3,4,5},O{9,10}
 V1=0,V2=I,U1{4,1}
 J1,V1=V2^I
